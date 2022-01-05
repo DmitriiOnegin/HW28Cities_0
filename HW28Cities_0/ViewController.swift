596839adf44a79e3dbd19478f89f1973ccaafc6c
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-05 16:27:36 +0300

    Initial Commit

---
+++ HW28Cities_0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW28Cities_0
+//
+//  Created by Dmitrii Onegin on 05.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
