5fa95c1a03f3f4a6f35c364e98303e3be3909aaa
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-05 16:45:03 +0300

    add vc2

--- HW28Cities_0/ViewController.swift
+++ HW28Cities_0/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    let vc1 = "vc1"
+    let vc2 = "vc2"
 }
 
