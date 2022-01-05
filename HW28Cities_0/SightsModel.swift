21a30532d0b0a65fa6d1f4d27b5042cb65dd0254
Vlad <vlad14196@gmail.com>
2022-01-05 19:00:37 +0300

    Versio1

---
+++ HW28Cities_0/SightsModel.swift
@@ -0,0 +1,19 @@
+//
+//  SightsModel.swift
+//  HW28Cities_0
+//
+//  Created by user on 05.01.2022.
+//
+
+import Foundation
+
+struct Sights {
+    let name: String
+    let image: String
+    let adress: String
+    let rating: Int
+    let monuments: String
+    let entertainment: String
+    let withChildren: Bool
+    let category = ["Sport", "Nuture", "Museum", "Park", "Food"]
+}
